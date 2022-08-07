6627719b5848886dd9e1618c159a461c710b0b82
matovu-farid <matovu90@gmail.com>
2022-08-07 20:25:44 +0300

    Add none

--- src/server/db/room.ts
+++ src/server/db/room.ts
@@ -1,55 +1,54 @@
 import Room from "../../Interfaces/Room";
 import { prisma } from "./client";
-export  async function createroom(room: Room) {
-  const createdRoom = prisma.room.create({
-    data: {
-      name: room.name,
-      path: room.path,
-      image: room.image,
-      serverId: room.serverId
-    },
-    
-  });
+export async function createroom(room: Room) {
+	const createdRoom = prisma.room.create({
+		data: {
+			name: room.name,
+			path: room.path,
+			image: room.image,
+			serverId: room.serverId,
+		},
+	});
 
-  return await createdRoom;
+	return await createdRoom;
 }
-export async function getRooms(userId:string) {
-  return prisma.room.findMany({
-    where: {
-      members: {
-        some: {
-          id: userId
-        }
-      }
-    }
-  })
-  
+export async function getRooms(userId: string) {
+	return prisma.room.findMany({
+		where: {
+			members: {
+				some: {
+					id: userId,
+				},
+			},
+		},
+	});
 }
-export async function getUsersInRoom(roomId:string){
-  const users = prisma.user.findMany({
-    where:{
-      rooms: {
-        some: {
-          id: roomId
-        }
-      }
-    }
-  })
-  return users;
+export async function getUsersInRoom(roomId: string) {
+	const users = prisma.user.findMany({
+		where: {
+			rooms: {
+				some: {
+					id: roomId,
+				},
+			},
+		},
+	});
+	return users;
 }
-export async function addToRoom(userRoomSession:{userId:string, roomId:string}) {
-  prisma.room.update({
-    where: {
-      id: userRoomSession.roomId
-    },
-    data:{
-      members:{
-        connect:{
-          id: userRoomSession.userId
-        }
-      }
-    }
-  })
-
-  
+export async function addToRoom(userRoomSession: {
+	userId: string;
+	roomId: string;
+}) {
+	prisma.room.update({
+		where: {
+			id: userRoomSession.roomId,
+		},
+		data: {
+			members: {
+				connect: {
+					id: userRoomSession.userId,
+				},
+			},
+		},
+	});
 }
